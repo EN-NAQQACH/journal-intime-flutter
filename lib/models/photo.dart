d6b4d4a4d240a22ebce2b734c120d20c0a4b93c1
EN-NAQQACH <mohssineennaqqach0@gmail.com>
2025-12-25 15:17:37 +0100

    photo_model

--- lib/models/photo.dart
+++ lib/models/photo.dart
@@ -1,29 +1,46 @@
 class Photo {
-  int? id; // AUTOINCREMENT
-  int entryId; // Foreign key -> JournalEntry.id
-  String imagePath;
+  final int? id;
+  final int entryId;
+  final String imagePath;
+  final DateTime createdAt;
 
   Photo({
     this.id,
     required this.entryId,
     required this.imagePath,
-  });
+    DateTime? createdAt,
+  }) : createdAt = createdAt ?? DateTime.now();
 
-  // Convert object to Map (for SQLite)
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'entryId': entryId,
       'imagePath': imagePath,
+      'createdAt': createdAt.toIso8601String(),
     };
   }
 
-  // Convert Map to object (from SQLite)
   factory Photo.fromMap(Map<String, dynamic> map) {
     return Photo(
       id: map['id'],
       entryId: map['entryId'],
       imagePath: map['imagePath'],
+      createdAt: DateTime.parse(map['createdAt']),
+    );
+  }
+
+  
+  Photo copyWith({
+    int? id,
+    int? entryId,
+    String? imagePath,
+    DateTime? createdAt,
+  }) {
+    return Photo(
+      id: id ?? this.id,
+      entryId: entryId ?? this.entryId,
+      imagePath: imagePath ?? this.imagePath,
+      createdAt: createdAt ?? this.createdAt,
     );
   }
 }
